660c0c596a95d04f6beb732c6c5fa8d91141f93c
poornachandu2001 <99313312+poornachandu2001@users.noreply.github.com>
2022-03-16 15:12:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("skill safari")
+var str = "skill safari"
+
+// length of the string
+console.log(str.length)
+
+//change the string into uppercase letters
+console.log(str.toUpperCase())
+
+//change the string into lowercase letters
+console.log(str.toLowerCase())
+
+//finding the index position of a letter
+console.log(str.indexOf("a"))
+
+//finding the letter of a index position
+console.log(str[3])
+console.log(str[4])
+console.log(str[5])
+
+//To eliminate the quotations inside another we use "\"
+console.log("Ironman says \"The Truth is… I am Iron Man.\"")
+
+//"\n" for new line
+console.log("Captain America says \"I don’t like bullies; I don’t care where they’re from.\” \nIronman says \"It's good to meet you, Dr. Banner. Your work on anti-electron collisions is unparalleled.\" \ncaptain america says \"I'm just a kid from Brooklyn.\”")
+
+//Body mass index
+var hei = 10
+var wei = 85
+var pro = hei*hei
+var bmi = wei/pro
+console.log(bmi)
+console.log(Math.round(bmi)) // rounds off to nearest integer
+console.log(Math.ceil(bmi))  // rounds up to integer
+console.log(Math.floor(bmi)) // rounds down to integer
